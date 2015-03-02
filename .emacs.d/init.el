cd0f7478451c3763915238f9f1291d8057225acd
Adam Zelei <za1111@hszk.bme.hu>
2015-03-02 18:50:51 +0100

    add emacs configuration

---
+++ .emacs.d/init.el
@@ -0,0 +1,33 @@
+(package-initialize)
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(blink-cursor-mode nil)
+ '(column-number-mode t)
+ '(custom-enabled-themes (quote (zenburn)))
+ '(custom-safe-themes
+   (quote
+    ("cbef37d6304f12fb789f5d80c2b75ea01465e41073c30341dc84c6c0d1eb611d" "9dae95cdbed1505d45322ef8b5aa90ccb6cb59e0ff26fef0b8f411dfc416c552" "de538b2d1282b23ca41ac5d8b69c033b911521fe27b5e1f59783d2eb20384e1f" default)))
+ '(desktop-save-mode t)
+ '(evil-mode t)
+ '(global-hl-line-mode t)
+ '(global-linum-mode t)
+ '(global-undo-tree-mode t)
+ '(helm-mode t)
+ '(menu-bar-mode nil)
+ '(mouse-wheel-mode nil)
+ '(package-archives
+   (quote
+    (("melpa" . "http://melpa.org/packages/")
+     ("gnu" . "http://elpa.gnu.org/packages/"))))
+ '(scroll-bar-mode nil)
+ '(tool-bar-mode nil))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(default ((t (:weight normal :height 120 :width normal :foundry "adobe" :family "Source Code Pro")))))
