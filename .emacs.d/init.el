e41c7dfa753b1f0d882233c16aca8d42257b085d
Adam Zelei <za1111@hszk.bme.hu>
2015-07-25 19:31:13 +0200

    add yank till the end of line function and bind it to the Y key

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -3,9 +3,6 @@
 (package-initialize)
 (when (not package-archive-contents) (package-refresh-contents))
 (mapc (lambda (package) (unless (package-installed-p package) (package-install package))) '(evil flycheck monokai-theme undo-tree web-mode))
-(load-theme 'monokai t)
-(require 'web-mode)
-(add-to-list 'auto-mode-alist '("\\.html?\\'" . web-mode))
 
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
@@ -27,10 +24,23 @@
  '(scroll-bar-mode nil)
  '(tool-bar-mode nil)
  '(undo-tree-auto-save-history t)
- '(web-mode-markup-indent-offset 2))
+ '(web-mode-markup-indent-offset 2)
+ '(word-wrap t))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(default ((t (:weight normal :height 120 :width normal :foundry "adobe" :family "Source Code Pro")))))
+
+(load-theme 'monokai t)
+(require 'web-mode)
+(add-to-list 'auto-mode-alist '("\\.html?\\'" . web-mode))
+
+(defun prelude-yank-to-end-of-line ()
+  "Yank to end of line."
+  (interactive)
+  (evil-yank (point) (point-at-eol)))
+
+(define-key evil-normal-state-map
+  (kbd "Y") 'prelude-yank-to-end-of-line)
