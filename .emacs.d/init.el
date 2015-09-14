b7cca030d6343d4ae4bd4995725b01c858deeb38
Adam Zelei <za1111@hszk.bme.hu>
2015-09-15 01:14:04 +0200

    add a bunch of great plugins

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -2,7 +2,7 @@
 (add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/"))
 (package-initialize)
 (when (not package-archive-contents) (package-refresh-contents))
-(mapc (lambda (package) (unless (package-installed-p package) (package-install package))) '(evil flycheck monokai-theme powerline undo-tree web-mode))
+(mapc (lambda (package) (unless (package-installed-p package) (package-install package))) '(company evil flycheck js2-mode monokai-theme powerline rainbow-delimiters undo-tree web-mode))
 
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
@@ -19,14 +19,21 @@
  '(indent-tabs-mode nil)
  '(initial-major-mode (quote ruby-mode))
  '(initial-scratch-message nil)
+ '(js2-basic-offset 2)
  '(menu-bar-mode nil)
  '(powerline-default-separator (quote slant))
  '(powerline-default-separator-dir (quote (right . right)))
+ '(powerline-display-buffer-size nil)
+ '(powerline-display-hud nil)
+ '(powerline-display-mule-info nil)
  '(scroll-bar-mode nil)
  '(show-paren-mode t)
  '(tool-bar-mode nil)
  '(undo-tree-auto-save-history t)
+ '(web-mode-code-indent-offset 2)
+ '(web-mode-css-indent-offset 2)
  '(web-mode-enable-auto-closing t)
+ '(web-mode-enable-auto-quoting t)
  '(web-mode-markup-indent-offset 2)
  '(word-wrap t))
 (custom-set-faces
@@ -44,20 +51,17 @@
 
 (require 'powerline)
 (powerline-default-theme)
-(setq powerline-display-buffer-size nil)
-(setq powerline-display-mule-info nil)
-(setq powerline-display-hud nil)
 
 (require 'web-mode)
 (add-to-list 'auto-mode-alist '("\\.html?\\'" . web-mode))
 
-(define-key evil-normal-state-map
-  "za" 'web-mode-fold-or-unfold)
-
-(defun prelude-yank-to-end-of-line ()
+(defun yank-to-end-of-line ()
   "Yank to end of line."
   (interactive)
   (evil-yank (point) (point-at-eol)))
 
 (define-key evil-normal-state-map
-  "Y" 'prelude-yank-to-end-of-line)
+  "Y" 'yank-to-end-of-line)
+
+(add-to-list 'auto-mode-alist '("\\.es6\\'" . js2-mode))
+(add-to-list 'auto-mode-alist '("\\.js\\'" . js2-mode))
